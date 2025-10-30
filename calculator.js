5d5f0c9d3f6d1ab4bc215f2c2c4405c438a6f839
kiwaeva <kiwaeva@gmail.com>
2025-10-30 17:16:13 +0000

    Fill out each function with the solution.

--- calculator.js
+++ calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
 const multiply = function() {
-
+    return array.reduce((total, current)=> total * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1; //start at 1 
+    for (let i = 1; i <= num; i++){
+        total = total * i;
+    }
 };
 
 // Do not edit below this line
