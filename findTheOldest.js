f6aacc54e56b9338a1173aebcfea1b64d2d9bbaf
kiwaeva <kiwaeva@gmail.com>
2025-11-15 10:00:47 +0000

    Write another solution, more modern, short and clean.

--- findTheOldest.js
+++ findTheOldest.js
@@ -1,20 +1,26 @@
-const getAge = function (birth, death){
-    if(!death) {
-        death = new Date().getFullYear();
-    }
-    return death - birth;
-}
+// const getAge = function (birth, death){
+//     if(!death) {
+//         death = new Date().getFullYear();
+//     }
+//     return death - birth;
+// }
 
-const findTheOldest = function(people) {
-     return people.reduce((oldest, currentPerson) =>{
-        const oldestAge = getAge (oldest.yearOfBirth, oldest.yearOfDeath);
-        const currentAge = getAge(
-            currentPerson.yearOfBirth,
-            currentPerson.yearOfDeath
-        );
-        return oldestAge < currentAge ? currentPerson : oldest
-     })
-};
+// const findTheOldest = function(people) {
+//      return people.reduce((oldest, currentPerson) =>{
+//         const oldestAge = getAge (oldest.yearOfBirth, oldest.yearOfDeath);
+//         const currentAge = getAge(
+//             currentPerson.yearOfBirth,
+//             currentPerson.yearOfDeath
+//         );
+//         return oldestAge < currentAge ? currentPerson : oldest
+//      })
+// };
+
+const getAge = ({ yearOfBirth, yearOfDeath }) =>
+  (yearOfDeath ?? new Date().getFullYear()) - yearOfBirth;
+
+const findTheOldest = (people) =>
+  people.toSorted((a, b) => getAge(b) - getAge(a))[0];
 
 // Do not edit below this line
 module.exports = findTheOldest;
